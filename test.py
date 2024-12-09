c5add07f934439ae286cc45436fb67e2cae03c7f
pppB02 <pappbalazs02@gmail.com>
2024-12-09 12:41:59 +0100

    tk python progi

--- test.py
+++ test.py
@@ -4,10 +4,11 @@ def get_random_num(a: int,b: int):
     try:
         print(random.randint(a,b))
         print("A -B:",a - b)
+        for i in range(len(a))
     except:
         print("Elbasztad teso!")
 
 numA = int(input("A szam:"))
 numB = int(input("B szam:"))
 
-get_random_num(numA,numB)	
+get_random_num(numA,numB)
