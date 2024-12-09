8db6a08b5000eb26c94a2709aca7644edd48ec00
pppB02 <pappbalazs02@gmail.com>
2024-12-09 13:15:06 +0100

    uptade

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ def get_random_num(a: int,b: int):
     try:
         print(random.randint(a,b))
         print("A -B:",a - b)
-        for i in range(len(a))
     except:
         print("Elbasztad teso!")
 
